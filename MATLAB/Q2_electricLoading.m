c5d04e4b55bdafbb41c831cd94cfcefce32117cb
Barış Kuseyri <bariskuseyri93@gmail.com>
2020-05-02 02:07:26 +0300

    Update report
    Continuing on the report.
    MATLAB files are updated:
    -implemented effective axial length
    -started on plotting BH curve
    -verified electrical loading and tangential stress

--- MATLAB/Q2_electricLoading.m
+++ MATLAB/Q2_electricLoading.m
@@ -18,13 +18,16 @@ Ns = 12;    % number of slots
 pp = 2;     % number of pole-pairs
 p = pp*2;   % number of poles
 
-laxial = 0.100;     % axial length [m]
+
+
+l_m = 0.100;        % axial length [m]
 lg = 0.001;         % airgap clearance [m]
+l_e = l_m + 2*lg;   % axial effective length [m]
 
 Drotor_outer = 0.100;               % rotor diameter [m]
 Rrotor_outer = Drotor_outer/2;      % rotor radius [m]
 lm = 0.004;                         % magnet radial thickness [m]
-d = 0.61;                            % slot ratio
+d = 0.545;                           % slot ratio
 % d = 0.61 for A = 80k - max A with air cooling
 % d = 0.85 for stator diameter = 160mm
 
@@ -34,7 +37,7 @@ slotShape = 2;      % shape of slot
 % 2: trapezoidal slot rectangular tooth
                                     
 I = 2.5;        % coil current
-J = 5*1e6;      % maximum current density [A/m^2]
+J_max = 5*1e6;      % maximum current density [A/m^2]
 Kp = 0.6;       % maximum fill factor
 
 %% Analysis
@@ -64,48 +67,37 @@ elseif slotShape == 2
 else
 end
 
-A = (Ns*Kp*areaSlot*J)/(2*pi*Rslot_inner);
+%A = (Ns*Kp*areaSlot*J_max)/(2*pi*Rslot_inner);
 
 
 %% Number of turns, choosing a cable
 
-Is = Kp*areaSlot*J;      % slot current [A]
-Ncond = Is/2.5;     % number of conductors per slot
-aa = areaSlot*Kp;        % copper area [m^2]
-cond_area = aa/Ncond;   % single conductor area [m^2]
+Is = Kp*areaSlot*J_max;     % slot current [A]
+Ncond = Is/2.5;             % number of conductors per slot
+aa = areaSlot*Kp;           % copper area [m^2]
+cond_area = aa/Ncond;       % single conductor area [m^2]
 
 
 % a single conductor area should be 0.5mm2 or higher, as coil current is
 % 2.5 A, a conductor area lower than 0.5mm2 would exceed the max. current
 % density J=5A/mm2. Thus, the the AWG choice is 0.518mm2
 % AWG20: 0.518 mm^2
+A_cond = 0.518e-6;              % conductor area
 
-Ncond2 = Kp*areaSlot/0.518e-6;
-% 999 conductors per slot
+Ncond2 = Kp*areaSlot/A_cond;    % 999 conductors per slot
 
-A2=(Ns*floor(Ncond2)*I)/(2*pi*Rslot_inner);
+A=(1/sqrt(2))*(Ns*floor(Ncond2)*I)/(2*pi*Rslot_inner);  % specific electric loading
 
 %% Average tangential stress in the rotor surface
 
-sigma_tan = (A2*Bg_peak)/2;
-
-F = sigma_tan*2*pi*Rrotor_outer*laxial;
-
-T = 2*sigma_tan*((Rrotor_outer^2)*pi*laxial);
+sigma_tan = (A*Bg_peak)/sqrt(2);        % tangential stress [N/m^2]
+F = sigma_tan*2*pi*Rrotor_outer*l_e;    % total force [N]
 
 %% Power outcome
 
+T = 2*sigma_tan*((Rrotor_outer^2)*pi*l_e);      % rated torque [Nm]
 w = 1500;               % rotor speed [rpm]
 w_rad = w*(2*pi/60);    % rotor speed [rad/s]
 P = T*w_rad;            % power outcome [W]
-
-
-
-
-
-
-
-
-
 
 
